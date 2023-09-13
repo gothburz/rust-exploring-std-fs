95ec3ff585b51688d1e95f272f730c090b3a33ff
peterg <peter_girnus@trendmicro.com>
2023-09-13 17:12:48 -0500

    add comment in dir_builder_mod.rs

--- src/mods/dir_builder_mod/dir_builder_mod.rs
+++ src/mods/dir_builder_mod/dir_builder_mod.rs
@@ -8,6 +8,7 @@ pub fn create_directories(path_str: &str, permission_u32: u32) {
     builder.recursive(true);
     // Set the builder to assign UNIX permissions assigned by the function caller
     builder.mode(permission_u32);
+    // Flow control on path creation
     match builder.create(path_str) {
         Ok(()) => println!("The path {:?} was created!", path_str),
         Err(e) => println!("ERROR: {:?}", e)
